021fec00df28866928d801778091f5a7c7cb1c57
AlexBezkopylnyi <sanok.bes@gmail.com>
2019-04-03 23:44:20 +0300

    Initial commit

---
+++ Pages/Jewels-and-Stones.xcplaygroundpage/Contents.swift
@@ -0,0 +1,12 @@
+
+// Count a number of characters in string J contains in a string S
+//
+// https://leetcode.com/problems/jewels-and-stones/
+import Foundation
+
+func numJewelsInStones(_ J: String, _ S: String) -> Int {
+    var count = 0
+   
+    return count
+}
+
