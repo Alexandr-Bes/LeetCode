bc46b8145203862a01702cb9964deac4649ba945
AlexBezkopylnyi <sanok.bes@gmail.com>
2019-06-05 22:02:50 +0300

    Folder updates

---
+++ Pages/To-Lower-Case.xcplaygroundpage/Contents.swift
@@ -0,0 +1,14 @@
+
+// Returns the same string in lowercase.
+//
+// https://leetcode.com/problems/to-lower-case/
+
+import Foundation
+
+class Solution {
+    func toLowerCase(_ str: String) -> String {
+        let lowerstr = str.lowercased()
+        return lowerstr
+    }
+}
+
